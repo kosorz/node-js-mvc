df726f65f3d12f2fac3e57447e3a93616eadfc6d
kosorz <a.kosorz92@gmail.com>
2020-03-23 20:48:31 +0100

    Add orders route, enhance js syntax, add mobile nav, public/js, update markup and styles

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,25 +1,25 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { 
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product', 
-    });
-}
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    const { title, imageUrl, price, description } = req.body;
-    const product = new Product(title, imageUrl, description, price);
-    product.save();
-    res.redirect('/');
-}
+  const { title, imageUrl, description, price } = req.body;
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-            prods: products, 
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
-        });
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
-}
+  });
+};
