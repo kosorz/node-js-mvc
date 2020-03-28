80348eb894a611e60e0de567c2131337eaf35c84
kosorz <a.kosorz92@gmail.com>
2020-03-28 19:27:13 +0100

    Add possibility to delete products

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart')
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -17,22 +19,51 @@ const getProductsFromFile = cb => {
   });
 };
 
+const saveData = (data, cb) => {
+  fs.writeFile(p, JSON.stringify(data), cb);
+}
+
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
   }
 
-  save() {
+  save(cb) {
     getProductsFromFile(products => {
-      this.id = Math.random().toString()
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        saveData(updatedProducts, (err) => {
+          console.log(err)
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        saveData(products, (err) => {
+          if (!err) {
+            cb();
+          }
+        });
+      }
     });
+  }
+
+  static deleteById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id)
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      saveData(updatedProducts, (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+          cb();
+        }
+      });
+    })
   }
 
   static fetchAll(cb) {
